91dcf711c6a253ad550bf57167ae741e9a49e610
Jeevan Daka <jeevandaka@gmail.com>
2025-06-29 01:41:39 +0530

    Initial Commit of spring boot project

---
+++ src/main/java/com/example/SpringAsClient/SpringAsClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringAsClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAsClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAsClientApplication.class, args);
+	}
+
+}
